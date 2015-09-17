355695c951d66de4ea3a4d03b2b5d89794f4e3c8
Norbert Kiesel <nkiesel@metricstream.com>
2015-09-17 16:47:07 -0700

    Simplified code

--- tools/conv.py
+++ tools/conv.py
@@ -10,33 +10,28 @@ cardinals = {
 
 formats = ('dms', 'noaa', 'dmm')
 
-def conv(fmt, ds, l):
+def conv(ds, l):
     dd = float(ds)
     cardinal = cardinals[l][dd >= 0]
-    dd = abs(dd)
+    minutes, seconds = divmod(abs(dd) * 3600, 60)
+    degrees, minutes = divmod(minutes, 60)
     if fmt == 'dms':
-        minutes, seconds = divmod(dd * 3600, 60)
-        degrees, minutes = divmod(minutes, 60)
-        return '"{0:s}{1:.0f} {2:.0f} {3:.3f}"'.format(cardinal, degrees, minutes, seconds)
+        return '{0:s}{1:.0f} {2:.0f} {3:.3f}'.format(cardinal, degrees, minutes, seconds)
     if fmt == 'noaa':
-        minutes, seconds = divmod(dd * 3600, 60)
-        degrees, minutes = divmod(minutes, 60)
-        return '"{1:.0f}-{02:.0f}-{3:06.3f}{0:s}"'.format(cardinal, degrees, minutes, seconds)
+        return '{1:.0f}-{02:.0f}-{3:06.3f}{0:s}'.format(cardinal, degrees, minutes, seconds)
     if fmt == 'dmm':
-        degrees, minutes = divmod(dd * 60, 60)
-        return '"{0:s}{1:.0f} {2:.6f}"'.format(cardinal, degrees, minutes)
+        return '{0:s}{1:.0f} {2:.6f}'.format(cardinal, degrees, minutes + seconds / 60)
     return ds
-    
-def coords(match):
-    return '{},{},'.format(conv(f, match.group(1), 'lat'),
-                           conv(f, match.group(2), 'lon'))
 
-f = sys.argv[1]
-if not f in formats:
+def coords(match):
+    return '"{}","{}",'.format(conv(match.group(1), 'lat'), conv(match.group(2), 'lon'))
+
+fmt = sys.argv[1]
+if not fmt in formats:
     print('Unsupported format. Please use one of {}'.format(formats))
     exit(1)
 
-p = re.compile(r'^(-?\d{1,3}\.\d{6}),(-?\d{1,3}\.\d{6}),')
+p = re.compile(r'^([-.\d]+),([-.\d]+),')
 
 for line in sys.stdin:
     print(re.sub(p, coords, line), end='')
