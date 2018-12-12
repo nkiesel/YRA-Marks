0b41e5cedbb53df537276e63cb6d9eb8ae245680
Norbert Kiesel <nkiesel@metricstream.com>
2018-12-11 17:21:55 -0800

    Finish code for reading YRA buoy status

--- tools/buoy-status.py
+++ tools/buoy-status.py
@@ -4,20 +4,40 @@ import csv
 import re
 import requests
 
-p = re.compile(r'<strong>(\w+)</strong>\D+([\d .]+)\D+([\d .]+) \((.+)\)')
+p = re.compile(r'<strong>(\w+)</strong>\D+(\d+) ([\d.]+)\D+(\d+) ([\d.]+) \((.+)\)')
 
-dmm = {}
-for row in csv.reader(open('../San_Francisco.dmm')):
-   dmm[row[2]] = row
+yra = {}
+for row in csv.reader(open('../San_Francisco.csv')):
+   yra[row[2]] = row
+
+def dmm2ddd(d, m, c):
+   seconds = float(d) * 3600 + float(m) * 60
+   if c in ['S', 'W']:
+      seconds = -seconds
+   return "{:.6f}".format(seconds / 3600)
 
 with requests.get('http://yra.org/buoy-status/') as response:
    for line in response.iter_lines(decode_unicode=True):
       m = p.match(line)
       if m:
          mark = "YRA-{}".format(m.group(1))
-         if mark not in dmm:
-            print('"N{}","W{}","{}","{}"'.format(m.group(2), m.group(3), mark, m.group(4)))
-#            print(dmm[mark])
+         lat = dmm2ddd(m.group(2), m.group(3), 'N')
+         lon = dmm2ddd(m.group(4), m.group(5), 'W')
+#         print('"{}","{}","{}","{}"'.format(lat, lon, mark, m.group(6)))
+#         print(yra[mark])
+         if mark in yra:
+            x = yra[mark]
+            if not re.match(r'\[\d+\]', x[3]):
+               dlat = abs(float(lat) - float(x[0]))
+               dlon = abs(float(lon) - float(x[1]))
+               if dlat > 0.0001 or dlon > 0.0001:
+                  print('Position for mark {} is off by {} lat and {} lon'.format(mark, dlat, dlon))
+                  print('"{}","{}","{}","{}"'.format(lat, lon, mark, m.group(6)))
+                  print(yra[mark])
+         else:
+            print('"{}","{}","{}","{}"'.format(lat, lon, mark, m.group(6)))
+                  
+#            print('"N{} {}","W{} {}","{}","{}"'.format(m.group(2), m.group(3), m.group(4), m.group(5), mark, m.group(6)))
 
 
 # next steps are
