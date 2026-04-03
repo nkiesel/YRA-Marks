c90a5804c7f5101431124aa2edd994165d349157
Norbert Kiesel <nk@iname.com>
2026-04-03 15:13:31 -0700

    moved NoaaMark into separate file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
@@ -35,72 +34,7 @@ val httpClient = OkHttpClient()
 @Serializable
 data class Mapping(val entries: Map<String, String>, val urls: List<String>)
 
-@Serializable
-data class NoaaMark(
-    val features: List<Feature>,
-    val type: String
-) {
-    @Serializable
-    data class Feature(
-        val id: Int,
-        val geometry: Geometry,
-        val properties: Properties,
-    ) {
-        @Serializable
-        data class Geometry(
-            val coordinates: List<Double>,
-        )
-
-        @Serializable
-        data class Properties(
-            @SerialName("LIGHT_LIST_NUMBER")
-            val lightListNumber: Double,
-            @SerialName("ASSIGNED_LATITUDE")
-            val assignedLatitude: String,
-            @SerialName("ASSIGNED_LONGITUDE")
-            val assignedLongitude: String,
-            @SerialName("COLOR")
-            val color: String?,
-            @SerialName("DAYMARK_COLOR")
-            val daymarkColor: String?,
-            @SerialName("DAYMARK_REMARK")
-            val daymarkRemark: String?,
-            @SerialName("DAYMARK_SHAPE")
-            val daymarkShape: String?,
-            @SerialName("DECIMAL_LATITUDE")
-            val decimalLatitude: Double,
-            @SerialName("DECIMAL_LONGITUDE")
-            val decimalLongitude: Double,
-            @SerialName("DESCRIPTION_TYPE")
-            val descriptionType: String,
-            @SerialName("LIGHT_CHAR")
-            val lightChar: String?,
-            @SerialName("NAME")
-            val name: String,
-            @SerialName("SECONDARY_UNIT_NAME")
-            val secondaryUnitName: String?,
-        )
-
-        fun toMark(id: String, names: Map<String, String>): Mark {
-            val color = properties.color.takeUnless { it == "N/A" } ?: digitsRegex.find(id)?.value?.toInt()
-                ?.let { if (it % 2 == 0) "R" else "G" }
-            val description = listOf(
-                color,
-                nameRegex.find(properties.name)?.groupValues?.get(1)?.let { "'$it'" },
-                properties.lightChar,
-                trimAll(properties.name),
-                names[id]?.let { "($it)" },
-            ).filterNot { it.isNullOrEmpty() || it == "N/A" }.joinToString(" ") { it!!.trim() }
-            return Mark(
-                id,
-                properties.lightListNumber.toString(),
-                Latitude.ofDegrees(geometry.coordinates[1]),
-                Longitude.ofDegrees(geometry.coordinates[0]),
-                description,
-            )
-        }
-    }
-}
+fun Path.toMapping() = Yaml.default.decodeFromString(Mapping.Companion.serializer(), readText())
 
 data class Mark(
     val id: String,
@@ -233,8 +167,6 @@ fun fetchMarksFromNOAA(yra: Mapping, noaa: Mapping): List<Mark> {
     }
 }
 
-fun getMapping(path: Path): Mapping = Yaml.default.decodeFromString(Mapping.serializer(), path.readText())
-
 const val baseName = "San_Francisco"
 
 typealias Marks = Map<String, Mark>
@@ -270,11 +202,10 @@ fun writeGpx(marks: Marks) {
     GPX.write(gpx, Path("$baseName.gpx"))
 }
 
-
 fun main(): Unit = runBlocking {
     val csv = Path("$baseName.csv")
-    val yra = getMapping(Path("yra.yaml"))
-    val noaa = getMapping(Path("noaa.yaml"))
+    val yra = Path("yra.yaml").toMapping()
+    val noaa = Path("noaa.yaml").toMapping()
 
     // Fetch concurrently
     withContext(Dispatchers.IO) {
